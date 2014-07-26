548a73bb92050d5616f7adf5b7661ab40e95cba9
VitaliyOliynyk <xaoc00@gmail.com>
2014-07-26 23:45:42 +0200

    initial commit

--- index.php
+++ index.php
@@ -5,5 +5,25 @@
 </head>
 <body>
 
+<form method="get">
+    <div>
+        arg1: <input type="text" name="arg1"/> arg2:<input type="text" name="arg2"/>
+    </div>
+    <select name="operation">
+        <option name="add">add</option>
+        <option name="remove">remove</option>
+        <option name="multiply">multiply</option>
+        <option name="divide">divide</option>
+    </select>
+    <div>
+        <input type="submit" value="calculate"/>
+    </div>
+    <?php
+    if($_GET['arg1']) {
+        echo "Result:" ;
+    }
+    ?>
+</form>
+
 </body>
 </html>
